1e6f8eaa5cb33e32dd8ba8b85bc8bfd7fedd67ff
Alvin Galit <aroblesgalit@gmail.com>
2024-02-10 13:16:24 -0800

    Created component for a todo item

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
+import { prisma } from "@/db";
 import Link from "next/link";
+import TodoItem from "./components/TodoItem";
 
-export default function Home() {
+function getTodos() {
+  return prisma.todo.findMany()
+}
+
+export default async function Home() {
+
+  const todos = await getTodos();
+
   return <>
     <header className="flex justify-between mb-4 items-center">
       <h1 className="text-2xl">Todos</h1>
@@ -8,5 +17,10 @@ export default function Home() {
         className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
         href="/new">New</Link>
     </header>
+    <ul className="pl-4">
+      {todos.map(todo => (
+        <TodoItem key={todo.id} {...todo} />
+      ))}
+    </ul>
   </>
 }
